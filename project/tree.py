9c23f551232894873cc1c1f3a525cc7f2fa76bb1
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2020-06-13 12:48:36 -0600

    Firs round of code for tree structure

---
+++ project/tree.py
@@ -0,0 +1,48 @@
+"""
+Tree structure with basic methods built in
+"""
+
+from person import (Address, Person)
+
+class Node:
+    """
+    A single entity in the tree
+    """
+
+    def __init__(self, person):
+        pass
+
+    def __str__(self):
+        pass
+
+
+class Tree:
+    """
+    A tree representation of the COVID-19 data
+    """
+
+    def insert(self, node):
+        """
+        Inserts the node somewhere in the tree
+        """
+        pass
+
+    def search(self, node):
+        """
+        Find the node in the tree. Return empty node
+        if not found
+        """
+        pass
+
+    def get_affected_percentage(self):
+        """
+        return the percentage of affected nodes in the 
+        whole tree.
+        """
+        pass
+
+    def print_all_nodes(self):
+        """
+        Print all nodes recusively
+        """
+        pass
