9eeef061db13420a3cc58426ea40b00daff3178b
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2020-06-13 12:48:52 -0600

    Firs version of Person related classes

---
+++ project/person.py
@@ -0,0 +1,17 @@
+"""
+Person related classes
+"""
+
+class Address:
+    def __init__(self, street, city, state, zip_code):
+        pass
+
+class Person:
+    """A representation of a single person in the COVID-19 data"""
+
+    def __init__(self, first_name, last_name, address, covid_affected=False):
+        pass
+
+    def __str__(self):
+        pass
+
