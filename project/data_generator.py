fc6679532efef0cf93388a7bd4acd8ecca74fee3
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2020-06-13 21:04:41 -0600

    Working version of the tree gen

--- project/data_generator.py
+++ project/data_generator.py
@@ -6,35 +6,68 @@ File format:
      Covid_affected, [children ids]
 """
 from person import (Person, Address)
+from random import randrange
+from queue import Queue
+import logging
+
+
+log = logging.getLogger("main-logger")
+s = logging.StreamHandler()
+log.addHandler(s)
+logging.basicConfig(level=logging.CRITICAL)
+
+
+class PersonGenerator(Person):
+    """A person with children list to help with generation"""
+
+    def __init__(self, first_name, last_name, address):
+        super().__init__(first_name, last_name, address)
+        self.children = []
+
+    def add_child(self, person):
+        self.children.append(person)
 
 
 class Generator:
     """Base data generator"""
 
-    def __init__(self, name_file, addresses_file, num_nodes, max_children, min_children):
+    def __init__(self, name_file, addresses_file, num_nodes, min_children, max_children):
         self.first_names, self.last_names = self.name_parser(name_file)
         self.streets, self.cities, self.states = self.address_parser(
             addresses_file)
+        self.min_children = min_children
+        self.max_children = max_children
+        self.num_nodes = num_nodes
 
         # index in the information lists above, used in generate_person function
         self.index = 0
 
+    def generate_data(self):
         """
-        while x < num_nodes:
-            num_children = random(max, min)
-            root = call gen
-            while y < num_children: 
-                person.append(call generat_person)
-                x ++ 
+        Generates the tree data under the parameter of the init
+        """
 
-            root.children = person
+        root = self.generate_person()
+        # depth_limit = self.num_nodes // num_children
+        depth_limit = 3
 
-            ...
-            for each child: 
-                root = child ... repeat 
+        def generate_single_path(self, parent, current_depth):
+            log.debug("Depth:  {}".format(current_depth))
+            if current_depth >= depth_limit:
+                return None
 
+            num_children = randrange(self.min_children, self.max_children)
+            for _ in range(num_children):
+                parent.add_child(self.generate_person())
 
-            """
+            log.debug(f"\t Parent has {len(parent.children)} children")
+            for child_index in range(num_children):
+                generate_single_path(self,
+                     parent.children[child_index], current_depth+1)
+                log.debug(f"\t Finished child {child_index} path")
+
+        generate_single_path(self, root, 0)
+        return root
 
     def name_parser(self, filename):
         """
@@ -69,10 +102,9 @@ class Generator:
             helper = line.split(",")  # split the address at the comma
             streets.append(helper[0])
             cities.append(helper[1])
-            # rstrip to get rid of the \n char at the end of the string and strip to get 
+            # rstrip to get rid of the \n char at the end of the string and strip to get
             # rid of the whitespace surrounding the states
             states.append((helper[2].replace(" ", "")).rstrip())
-        print(len(states), len(streets), len(cities))
         return streets, cities, states
 
     def mark_affected(self, threshold_to_mark=10):
@@ -83,22 +115,21 @@ class Generator:
         Sample a (somewhat) unique person from the data passed in
         :returns: Person
         """
-        if self.index < len(self.first_names):
-            address = Address(
-                self.streets[self.index],
-                self.last_names[self.index],
-                self.states[self.index]
-            )
-            person = Person(
-                self.first_names[self.index],
-                self.last_names[self.index],
-                address
-            )
-            self.index += 1
-            return person
-        else:
-            print("No more people left to sample")
-            return None
+        if self.index > len(self.first_names):
+            raise ValueError("No more people left to sample")
+
+        address = Address(
+            self.streets[self.index],
+            self.last_names[self.index],
+            self.states[self.index]
+        )
+        person = PersonGenerator(
+            self.first_names[self.index],
+            self.last_names[self.index],
+            address
+        )
+        self.index += 1
+        return person
 
     def produce_csv(self, file_name):
         """
@@ -112,8 +143,16 @@ class Generator:
 
 
 if __name__ == "__main__":
-    """
-    create gen object (pass lists of names )  <-- tree is built but not marked
-    obj.mark_affected(10)
-    obj.produce_csv
-    """
+
+    generator = Generator("data/names_to_sample.txt",
+                          "data/addresses_to_sample.txt", 10, 1, 5)
+    root = generator.generate_data()
+
+
+    queue = Queue()
+    queue.put(root)
+    while not queue.empty():
+        node = queue.get()
+        for child in node.children:
+            print(f"\"{node.person_id}\" -> \"{child.person_id}\"")
+            queue.put(child)
