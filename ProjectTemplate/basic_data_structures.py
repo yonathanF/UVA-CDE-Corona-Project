fbac941c529f2f05985fe7852f3f92f01a7e2938
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2020-07-19 10:52:56 -0400

    starting to build the project template

---
+++ ProjectTemplate/basic_data_structures.py
@@ -0,0 +1,8 @@
+class Stack:
+    """A simple stack wrapper class around a Python list"""
+    pass
+
+
+class Queue:
+    """A simple implementation of a queue using a Python list"""
+    pass
---
+++ ProjectTemplate/graph.py
@@ -0,0 +1,22 @@
+"""
+Graph structure with basic methods built in
+"""
+
+from personal_information import (Address, Person, COVID_Status)
+from basic_data_structures import (Stack, Queue)
+import csv
+import random
+
+
+class Node:
+    """
+    A single entity in the graph
+    """
+    pass
+
+
+class Graph:
+    """
+    A graph representation of the COVID-19 data
+    """
+    pass
