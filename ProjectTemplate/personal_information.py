c5340fb98979864fbe22aaacec5ac53d4066b8fc
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2020-07-23 14:59:06 -0400

    final version of the project template the students recieved

--- ProjectTemplate/personal_information.py
+++ ProjectTemplate/personal_information.py
@@ -6,34 +6,13 @@ from enum import Enum
 
 
 class COVID_Status(Enum):
-    AFFECTED = 1
-    NOT_AFFECTED = 2
-    UNKNOWN = 3
+    pass
 
 
 class Address:
-    def __init__(self, street, city, state):
-        self.street = street
-        self.city = city
-        self.state = state
-
-    def __str__(self):
-        return "{}, {}, {}".format(self.street, self.city, self.state)
+    pass
 
 
 class Person:
     """A representation of a single person in the COVID-19 data"""
-
-    def __init__(self, person_id,first_name, last_name, address, covid_affected=COVID_Status.UNKNOWN):
-        self.person_id = person_id
-        self.first_name = first_name
-        self.last_name = last_name
-        self.address = address
-        self.covid_affected = covid_affected
-
-    def is_person_affected(self):
-        return self.covid_affected is COVID_Status.AFFECTED
-
-    def __str__(self):
-        return "{}, {}, {}, {}, {},".format(self.person_id, self.first_name, self.last_name, str(self.address), self.covid_affected.name)
-
+    pass
